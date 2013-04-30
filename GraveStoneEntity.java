b4c2a020e7f61140aed4485ad179a13182dee0bc
NightKosh <nightmare@railfox.ru>
2013-04-30 19:19:21 +0400

    fix undead pets eggs color

--- GraveStoneEntity.java
+++ GraveStoneEntity.java
@@ -4,16 +4,12 @@ import GraveStone.entity.EntityZombieCat;
 import GraveStone.entity.EntityZombieDog;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityEggInfo;
-import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class GraveStoneEntity {
 
     private static GraveStoneEntity instance;
-    private static int startEntityId = 300;
 
     private GraveStoneEntity() {
         getEntity();
@@ -25,19 +21,18 @@ public class GraveStoneEntity {
         } else {
             return instance;
         }
-
     }
 
     public void getEntity() {
         // zombie dog
-        EntityRegistry.registerGlobalEntityID(EntityZombieDog.class, "GSZombieDog", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0x000000);
+        EntityRegistry.registerGlobalEntityID(EntityZombieDog.class, "GSZombieDog", EntityRegistry.findGlobalUniqueEntityId(), 14144467, 7969893);
         EntityRegistry.addSpawn(EntityZombieDog.class, 3, 4, 8, 
                 EnumCreatureType.monster, BiomeGenBase.forest, BiomeGenBase.forestHills, 
                 BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
         LanguageRegistry.instance().addStringLocalization("entity.GSZombieDog.name", "Zombie Dog");
 
         // zombie cat
-        EntityRegistry.registerGlobalEntityID(EntityZombieCat.class, "GSZombieCat", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0x000000);
+        EntityRegistry.registerGlobalEntityID(EntityZombieCat.class, "GSZombieCat", EntityRegistry.findGlobalUniqueEntityId(), 15720061, 7969893);
         EntityRegistry.addSpawn(EntityZombieCat.class, 3, 4, 8, 
                 EnumCreatureType.monster, BiomeGenBase.jungle, BiomeGenBase.jungleHills);
         LanguageRegistry.instance().addStringLocalization("entity.GSZombieCat.name", "Zombie Cat");
