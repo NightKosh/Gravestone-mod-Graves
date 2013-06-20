4e8d01150dbd8c5eb7f3792395ea796b1efbc51c
NightKosh <nightmare@railfox.ru>
2013-06-20 18:13:41 +0400

    reduce undead pets count

--- GraveStoneEntity.java
+++ GraveStoneEntity.java
@@ -36,7 +36,7 @@ public class GraveStoneEntity {
         // zombie dog
         EntityRegistry.registerGlobalEntityID(EntityZombieDog.class, "GSZombieDog", EntityRegistry.findGlobalUniqueEntityId(), 14144467, 7969893);
         if (GraveStoneConfig.spawnZombieDogs) {
-            EntityRegistry.addSpawn(EntityZombieDog.class, 3, 4, 8, EnumCreatureType.monster,
+            EntityRegistry.addSpawn(EntityZombieDog.class, 3, 1, 3, EnumCreatureType.monster,
                     BiomeGenBase.forest, BiomeGenBase.forestHills,
                     BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
         }
@@ -45,7 +45,7 @@ public class GraveStoneEntity {
         // zombie cat
         EntityRegistry.registerGlobalEntityID(EntityZombieCat.class, "GSZombieCat", EntityRegistry.findGlobalUniqueEntityId(), 15720061, 7969893);
         if (GraveStoneConfig.spawnZombieCats) {
-            EntityRegistry.addSpawn(EntityZombieCat.class, 3, 4, 8, EnumCreatureType.monster,
+            EntityRegistry.addSpawn(EntityZombieCat.class, 3, 1, 3, EnumCreatureType.monster,
                     BiomeGenBase.jungle, BiomeGenBase.jungleHills);
         }
         LanguageRegistry.instance().addStringLocalization("entity.GSZombieCat.name", "Zombie Cat");
@@ -54,7 +54,7 @@ public class GraveStoneEntity {
         // skeleton dog
         EntityRegistry.registerGlobalEntityID(EntitySkeletonDog.class, "GSSkeletonDog", EntityRegistry.findGlobalUniqueEntityId(), 14144467, 4802889);
         if (GraveStoneConfig.spawnSkeletonDogs) {
-            EntityRegistry.addSpawn(EntityZombieDog.class, 3, 4, 8, EnumCreatureType.monster,
+            EntityRegistry.addSpawn(EntityZombieDog.class, 3, 1, 3, EnumCreatureType.monster,
                     BiomeGenBase.forest, BiomeGenBase.forestHills,
                     BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
         }
@@ -63,7 +63,7 @@ public class GraveStoneEntity {
         // skeleton cat
         EntityRegistry.registerGlobalEntityID(EntitySkeletonCat.class, "GSSkeletonCat", EntityRegistry.findGlobalUniqueEntityId(), 15720061, 4802889);
         if (GraveStoneConfig.spawnSkeletonCats) {
-            EntityRegistry.addSpawn(EntityZombieCat.class, 3, 4, 8, EnumCreatureType.monster,
+            EntityRegistry.addSpawn(EntityZombieCat.class, 3, 1, 3, EnumCreatureType.monster,
                     BiomeGenBase.jungle, BiomeGenBase.jungleHills);
         }
         LanguageRegistry.instance().addStringLocalization("entity.GSSkeletonCat.name", "Skeleton Cat");
