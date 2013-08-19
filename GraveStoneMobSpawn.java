8507bef04fba91fe917596c3861792afa3493676
NightKosh <nightmare@railfox.ru>
2013-08-19 05:14:47 +0400

    rework and optimize forein mobs spawn. add scorpions from mocreatures to spauners

--- GraveStoneMobSpawn.java
+++ GraveStoneMobSpawn.java
@@ -30,19 +30,19 @@ public abstract class GraveStoneMobSpawn {
     /**
      * Provides a mapping between entity classes and a string
      */
-    private static Map mobNameToClassMapping = new HashMap();
+    private static Map<String, Constructor<EntityLiving>> mobNameToClassMapping = new HashMap();
     private static ArrayList<String> MOB_ID = new ArrayList(Arrays.asList("Zombie", "Skeleton"));
     private static ArrayList<String> DOG_ID = new ArrayList(Arrays.asList("GSZombieDog", "GSSkeletonDog"));
     private static ArrayList<String> CAT_ID = new ArrayList(Arrays.asList("GSZombieCat", "GSSkeletonCat"));
     private static ArrayList<String> HELL_MOB_ID = new ArrayList(Arrays.asList("PigZombie", "Skeleton"));
-    
+
     public enum EnumMobType {
+
         DEFAULT_MOBS,
         HELL_MOBS,
         UNDEAD_DOGS,
         UNDEAD_CATS
     }
-    
     // catacombs spawner mobs
     public static ArrayList<String> catacombsSpawnerMobs = new ArrayList(Arrays.asList(
             "Skeleton", "Spider", "Zombie",
@@ -50,12 +50,24 @@ public abstract class GraveStoneMobSpawn {
             "Skeleton", "Spider", "Zombie",
             "GSZombieDog", "GSZombieCat",
             "GSSkeletonDog", "GSSkeletonCat"));
-
-    // catacombs spawner mobs
+    // catacombs statues mobs
     public static ArrayList<String> catacombsStatuesMobs = new ArrayList(Arrays.asList(
             "Skeleton", "Zombie"));
-    /* 
+    // mo_creatures mobs classes
+    private static final String MO_CREATURES_S_SKELETON = "drzhark.mocreatures.entity.monster.MoCEntitySilverSkeleton";
+    private static final String MO_CREATURES_WRAITH = "drzhark.mocreatures.entity.monster.MoCEntityWraith";
+    private static final String MO_CREATURES_F_WRAITH = "drzhark.mocreatures.entity.monster.MoCEntityFlameWraith";
+    private static final String MO_CREATURES_SCORPIONS = "drzhark.mocreatures.entity.monster.MoCEntityScorpion";
+    // twilight forest mobs classes
+    private static final String TWILIGHT_WRAITH = "twilightforest.entity.EntityTFWraith";
+
+    /**
      * Check can grave spawn hell creature or not
+     *
+     * @param world
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @param z Z coordinate
      */
     private static boolean canSpawnHellCreatures(World world, int x, int y, int z) {
         if (world != null) {
@@ -82,17 +94,15 @@ public abstract class GraveStoneMobSpawn {
                 if (canSpawnHellCreatures(world, x, y, z) && world.rand.nextInt(20) == 0) {
                     id = getMobID(world.rand, EnumMobType.HELL_MOBS);
                     if (id.equals("Skeleton")) {
-                        EntitySkeleton skeleton = (EntitySkeleton) EntityList.createEntityByName(id, world);
+                        EntitySkeleton skeleton = getSkeleton(world);
                         skeleton.setSkeletonType(1);
-                        if (world.rand.nextInt(2) == 0) {
-                            skeleton.setCurrentItemOrArmor(0, new ItemStack(Item.swordStone.itemID, 1, 0));
-                        } else {
-                            skeleton.setCurrentItemOrArmor(0, new ItemStack(Item.bow.itemID, 1, 0));
-                        }
                         return skeleton;
                     }
                 } else {
                     id = getMobID(world.rand, EnumMobType.DEFAULT_MOBS);
+                    if (id.equals("Skeleton")) {
+                        return getSkeleton(world);
+                    }
                 }
         }
 
@@ -105,21 +115,53 @@ public abstract class GraveStoneMobSpawn {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return entity;
     }
 
-    private static EntityLiving getForeinMob(World world, String mobName) {
-        EntityLiving mob = null;
+    /**
+     * Return Skeleton with bow/sword
+     * @param world
+     * @return 
+     */
+    private static EntitySkeleton getSkeleton(World world) {
+        EntitySkeleton skeleton = (EntitySkeleton) EntityList.createEntityByName("Skeleton", world);
+        if (world.rand.nextInt(2) == 0) {
+            skeleton.setCurrentItemOrArmor(0, new ItemStack(Item.swordStone.itemID, 1, 0));
+        } else {
+            skeleton.setCurrentItemOrArmor(0, new ItemStack(Item.bow.itemID, 1, 0));
+        }
+        return skeleton;
+    }
 
+    /**
+     * Return constructor for forein mobs classes based on class path
+     * @param path Class Path with name
+     */
+    private static Constructor getForeinMobConstructor(String path) {
+        Constructor<EntityLiving> constructor = null;
         try {
-            Class mobClass = Class.forName((String) mobNameToClassMapping.get(mobName));
-            Constructor<EntityLiving> constructor = mobClass.getConstructor(World.class);
-            mob = constructor.newInstance(new Object[]{world});
+            Class mobClass = Class.forName(path);
+            constructor = mobClass.getConstructor(World.class);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
+        }
+
+        return constructor;
+    }
+
+    /**
+     * Create and return instance for forein mobs
+     * @param world
+     * @param mobName
+     */
+    private static EntityLiving getForeinMob(World world, String mobName) {
+        EntityLiving mob = null;
+
+        try {
+            mob = mobNameToClassMapping.get(mobName).newInstance(new Object[]{world});
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -131,8 +173,11 @@ public abstract class GraveStoneMobSpawn {
         return mob;
     }
 
-    /*
-     * return random mob id from list
+    /**
+     * Return random mob id from list
+     *
+     * @param random
+     * @param mobType
      */
     public static String getMobID(Random random, EnumMobType mobType) {
         switch (mobType) {
@@ -221,42 +266,59 @@ public abstract class GraveStoneMobSpawn {
         return random.nextInt(40) == 13;
     }
 
-    /*
+    /**
      * Add Mo'creatures mobs to mob list
      */
     public static void addMoCreaturesMobs() {
-        MOB_ID.add("SilverSkeleton");
-        mobNameToClassMapping.put("SilverSkeleton", "drzhark.mocreatures.entity.monster.MoCEntitySilverSkeleton");
-        catacombsSpawnerMobs.add("SilverSkeleton");
-        catacombsStatuesMobs.add("SilverSkeleton");
-        
-        MOB_ID.add("Wraith");
-        mobNameToClassMapping.put("Wraith", "drzhark.mocreatures.entity.monster.MoCEntityWraith");
-        catacombsSpawnerMobs.add("Wraith");
-        catacombsStatuesMobs.add("Wraith");
+        Constructor<EntityLiving> constructor = getForeinMobConstructor(MO_CREATURES_S_SKELETON);
 
-        HELL_MOB_ID.add("FlameWraith");
-        mobNameToClassMapping.put("FlameWraith", "drzhark.mocreatures.entity.monster.MoCEntityFlameWraith");
-        catacombsStatuesMobs.add("FlameWraith");
+        if (constructor != null) {
+            MOB_ID.add("SilverSkeleton");
+            mobNameToClassMapping.put("SilverSkeleton", constructor);
+            catacombsSpawnerMobs.add("SilverSkeleton");
+            catacombsStatuesMobs.add("SilverSkeleton");
+        }
+
+        constructor = getForeinMobConstructor(MO_CREATURES_WRAITH);
+        if (constructor != null) {
+            MOB_ID.add("Wraith");
+            mobNameToClassMapping.put("Wraith", constructor);
+            catacombsSpawnerMobs.add("Wraith");
+            catacombsStatuesMobs.add("Wraith");
+        }
+
+        constructor = getForeinMobConstructor(MO_CREATURES_F_WRAITH);
+        if (constructor != null) {
+            HELL_MOB_ID.add("FlameWraith");
+            mobNameToClassMapping.put("FlameWraith", constructor);
+            catacombsStatuesMobs.add("FlameWraith");
+        }
+
+        if (getForeinMobConstructor(MO_CREATURES_SCORPIONS) != null) {
+            catacombsSpawnerMobs.add("Scorpion");
+        }
     }
 
-    /*
+    /**
      * Add Mo'creatures mobs to mob list
      */
     public static void addTwilightForestMobs() {
-        MOB_ID.add("Twilight Wraith");
-        mobNameToClassMapping.put("Twilight Wraith", "twilightforest.entity.EntityTFWraith");
-        catacombsSpawnerMobs.add("Twilight Wraith");
-        catacombsStatuesMobs.add("Twilight Wraith");
+        Constructor<EntityLiving> constructor = getForeinMobConstructor(TWILIGHT_WRAITH);
+        if (constructor != null) {
+            MOB_ID.add("Twilight Wraith");
+            mobNameToClassMapping.put("Twilight Wraith", constructor);
+            catacombsSpawnerMobs.add("Twilight Wraith");
+            catacombsStatuesMobs.add("Twilight Wraith");
+        }
     }
-    
+
     /**
      * Return random mob for spawner
      */
     public static String getMobForSpawner(Random random) {
         return catacombsSpawnerMobs.get(random.nextInt(catacombsSpawnerMobs.size()));
     }
-    
+
     /**
      * Return random mob for spawner
      */
