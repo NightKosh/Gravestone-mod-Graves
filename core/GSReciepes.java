dd559f47a4b07d6c2e2244e32284cff765a96766
NightKosh <nightmare@railfox.ru>
2014-02-03 04:33:04 +0400

    add gravestone recieps

--- core/GSReciepes.java
+++ core/GSReciepes.java
@@ -2,8 +2,11 @@
 package gravestone.core;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import gravestone.block.GraveStoneHelper;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 /**
  * GraveStone mod
@@ -29,5 +32,35 @@ public class GSReciepes {
         // Bone block to bones
         GameRegistry.addRecipe(new ItemStack(Item.bone, 9), "x", 'x', GSBlock.boneBlock);
         
+        // graves
+        GameRegistry.addRecipe(getStackWithNTB(GSBlock.graveStone, (byte) 0, "GraveType"), "sc", 's', Block.stone, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getStackWithNTB(GSBlock.graveStone, (byte) 1, "GraveType"), " c", "s ", 's', Block.stone, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getStackWithNTB(GSBlock.graveStone, (byte) 2, "GraveType"), "c", "s", 's', Block.stone, 'c', GSItem.chisel);
+        // pet graves
+        GameRegistry.addRecipe(getStackWithNTB(GSBlock.graveStone, (byte) 3, "GraveType"), " c", "  ", "s ", 's', Block.stone, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getStackWithNTB(GSBlock.graveStone, (byte) 4, "GraveType"), " c", "  ", " s", 's', Block.stone, 'c', GSItem.chisel);
+        // sword graves
+        GameRegistry.addRecipe(getSwordGravestone(GSBlock.graveStone, (byte) 5), "sc", 's', Item.swordWood, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getSwordGravestone(GSBlock.graveStone, (byte) 6), "sc", 's', Item.swordStone, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getSwordGravestone(GSBlock.graveStone, (byte) 7), "sc", 's', Item.swordIron, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getSwordGravestone(GSBlock.graveStone, (byte) 8), "sc", 's', Item.swordGold, 'c', GSItem.chisel);
+        GameRegistry.addRecipe(getSwordGravestone(GSBlock.graveStone, (byte) 9), "sc", 's', Item.swordDiamond, 'c', GSItem.chisel);
+    }
+    
+    private static ItemStack getStackWithNTB(Block block, byte graveType, String ntbName) {
+        ItemStack stack = new ItemStack(block, 1, 0);
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setByte(ntbName, graveType);
+        stack.setTagCompound(nbt);
+        return stack;
+    }
+    
+    private static ItemStack getSwordGravestone(Block block, byte graveType) {
+        ItemStack stack = new ItemStack(block, 1, 0);
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setByte("GraveType", graveType);
+        nbt.setByte("SwordType", GraveStoneHelper.graveTypeToSwordType(graveType));
+        stack.setTagCompound(nbt);
+        return stack;
     }
 }
