daebaaf1688640601178e873e2e960cf906b6257
NightKosh <nightmare@railfox.ru>
2014-02-16 21:15:32 +0400

    skull candle model

--- models/block/ModelSkullCandle.java
+++ models/block/ModelSkullCandle.java
@@ -3,6 +3,7 @@ package gravestone.models.block;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import org.lwjgl.opengl.GL11;
 
 /**
  * GraveStone mod
@@ -12,91 +13,57 @@ import net.minecraft.entity.Entity;
  */
 public class ModelSkullCandle extends ModelBase {
 
-    public ModelRenderer Shape1;
-    public ModelRenderer Shape2;
-    public ModelRenderer Shape3;
-    public ModelRenderer Shape4;
-    public ModelRenderer Shape5;
-    public ModelRenderer Shape6;
-    public ModelRenderer Shape7;
-    public ModelRenderer Shape8;
-    public ModelRenderer Shape9;
+    private ModelRenderer Skull;
+    private ModelRenderer Teeth;
+    private ModelRenderer Candle;
+    private ModelRenderer Thread;
 
     public ModelSkullCandle() {
-        Shape1 = new ModelRenderer(this, 0, 0);
-        Shape1.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape1.setRotationPoint(-1F, 11F, 0.5F);
-        Shape1.setTextureSize(32, 32);
-        setRotation(Shape1, 0F, 0.7853982F, 0F);
-        Shape2 = new ModelRenderer(this, 0, 0);
-        Shape2.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape2.setRotationPoint(-0.25F, 11F, 1.2F);
-        Shape2.setTextureSize(32, 32);
-        Shape2.mirror = true;
-        setRotation(Shape2, 0F, 1.570796F, 0F);
-        Shape3 = new ModelRenderer(this, 0, 0);
-        Shape3.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape3.setRotationPoint(0.8F, 11F, 1.2F);
-        Shape3.setTextureSize(32, 32);
-        Shape3.mirror = true;
-        setRotation(Shape3, 0F, 2.356194F, 0F);
-        Shape4 = new ModelRenderer(this, 0, 0);
-        Shape4.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape4.setRotationPoint(0F, 11F, 0.5F);
-        Shape4.setTextureSize(32, 32);
-        Shape4.mirror = true;
-        setRotation(Shape4, 0F, 3.141593F, 0F);
-        Shape5 = new ModelRenderer(this, 0, 0);
-        Shape5.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape5.setRotationPoint(0.4F, 11F, -0.5F);
-        Shape5.setTextureSize(32, 32);
-        Shape5.mirror = true;
-        setRotation(Shape5, 0F, 3.926991F, 0F);
-        Shape6 = new ModelRenderer(this, 0, 0);
-        Shape6.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape6.setRotationPoint(1.5F, 11F, -0.5F);
-        Shape6.setTextureSize(32, 32);
-        Shape6.mirror = true;
-        setRotation(Shape6, 0F, 4.712389F, 0F);
-        Shape7 = new ModelRenderer(this, 0, 0);
-        Shape7.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape7.setRotationPoint(0.8F, 11F, -1.2F);
-        Shape7.setTextureSize(32, 32);
-        Shape7.mirror = true;
-        setRotation(Shape7, 0F, 5.497787F, 0F);
-        Shape8 = new ModelRenderer(this, 0, 0);
-        Shape8.addBox(0F, 0F, 0F, 1, 5, 1);
-        Shape8.setRotationPoint(-0.2F, 11F, -1.2F);
-        Shape8.setTextureSize(32, 32);
-        Shape8.mirror = true;
-        setRotation(Shape8, 0F, 0F, 0F);
-        Shape9 = new ModelRenderer(this, 0, 0);
-        Shape9.addBox(0F, 0F, 0F, 1, 1, 1);
-        Shape9.setRotationPoint(0F, 11F, -0.5F);
-        Shape9.setTextureSize(32, 32);
-        Shape9.mirror = true;
-        setRotation(Shape9, 0F, 0F, 0F);
+        textureWidth = 32;
+        textureHeight = 32;
+
+        Skull = new ModelRenderer(this, 0, 0);
+        Skull.addBox(0F, 0F, 0F, 8, 6, 8);
+        Skull.setRotationPoint(-4F, 16.6F, -4F);
+        setRotation(Skull, -0.1745329F, 0F, 0F);
+        Teeth = new ModelRenderer(this, 0, 14);
+        Teeth.addBox(0F, 0F, 0F, 4, 2, 2);
+        Teeth.setRotationPoint(-2F, 22F, -4.8F);
+        setRotation(Teeth, -0.1745329F, 0F, 0F);
+        Candle = new ModelRenderer(this, 0, 18);
+        Candle.addBox(0F, 0F, 0F, 2, 4, 2);
+        Candle.setRotationPoint(-1.5F, 13.5F, -1F);
+        setRotation(Candle, 0F, 0.7853982F, 0F);
+        Thread = new ModelRenderer(this, 16, 14);
+        Thread.addBox(0F, 0F, 0F, 1, 2, 1);
+        Thread.setRotationPoint(-0.7F, 12F, -2.5F);
     }
 
     /**
      * Sets the models various rotation angles then renders the model.
      */
     @Override
-    public void render(Entity entity, float par2, float par3, float par4, float par5, float par6, float par7) {
-        Shape1.render(par7);
-        Shape2.render(par7);
-        Shape3.render(par7);
-        Shape4.render(par7);
-        Shape5.render(par7);
-        Shape6.render(par7);
-        Shape7.render(par7);
-        Shape8.render(par7);
-        Shape9.render(par7);
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        Skull.render(f5);
+        Teeth.render(f5);
+        Candle.render(f5);
+        Thread.render(f5);
     }
 
     private void setRotation(ModelRenderer model, float x, float y, float z) {
         model.rotateAngleX = x;
         model.rotateAngleY = y;
         model.rotateAngleZ = z;
+    }
+    
+    public void renderAll() {
+        Skull.render(0.0625F);
+        Teeth.render(0.0625F);
+        Candle.render(0.0625F);
+        
+        GL11.glScalef(0.5F, 1, 0.5F);
+        Thread.render(0.0625F);
     }
 }
