3ee65a6317073037a78287553ade9cde94cb2a2f
NightKosh <nightmare@railfox.ru>
2014-02-21 03:59:29 +0400

    The camping mod compatibility

--- core/compatibility/GSCompatibilityTheCampingMod.java
+++ core/compatibility/GSCompatibilityTheCampingMod.java
@@ -1,5 +1,6 @@
 package gravestone.core.compatibility;
 
+import java.util.LinkedList;
 import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -15,38 +16,38 @@ import net.minecraft.nbt.NBTTagList;
 public class GSCompatibilityTheCampingMod {
 
     protected static boolean isTheCampingModInstalled = false;
-    
+
     private GSCompatibilityTheCampingMod() {
     }
 
     public static void addItems(List<ItemStack> items, EntityPlayer player) {
         if (isInstalled()) {
-            NBTTagCompound tag = player.getEntityData().getCompoundTag("campInv");
-            NBTTagList inventory = tag.getTagList("Items");
-            for (int i = 0; i < inventory.tagCount(); ++i) {
-                NBTTagCompound Slots = (NBTTagCompound) inventory.tagAt(i);
-                Slots.getByte("Slot");
-                items.add(ItemStack.loadItemStackFromNBT(Slots));
-            }
+            items.addAll(getItems(player));
         }
     }
-//
-//    public static void addItems(List<ItemStack> items, EntityPlayer player) {
-//        addItems(items, player, "backpack");
-//        addItems(items, player, "tool");
-//    }
-//
-//    private static void addItems(List<ItemStack> items, EntityPlayer player, String tagName) {
-//        NBTTagCompound tag = player.getEntityData().getCompoundTag("campInv").getCompoundTag(tagName);
-//        NBTTagList inventory = tag.getTagList("Items");
-//        for (int i = 0; i < inventory.tagCount(); ++i) {
-//            NBTTagCompound Slots = (NBTTagCompound) inventory.tagAt(i);
-//            Slots.getByte("Slot");
-//            items.add(ItemStack.loadItemStackFromNBT(Slots));
-//        }
-//    }
-    
-    
+
+    private static List<ItemStack> getItems(EntityPlayer player) {
+        List<ItemStack> items = new LinkedList<ItemStack>();
+
+        items.addAll(addItems(player, "backpack"));
+        items.addAll(addItems(player, "tool"));
+        
+        return items;
+    }
+
+    private static List<ItemStack> addItems(EntityPlayer player, String tagName) {
+        List<ItemStack> items = new LinkedList<ItemStack>();
+        NBTTagCompound tag = player.getEntityData().getCompoundTag("campInv").getCompoundTag(tagName);
+        NBTTagList inventory = tag.getTagList("Items");
+        for (int i = 0; i < inventory.tagCount(); ++i) {
+            NBTTagCompound Slots = (NBTTagCompound) inventory.tagAt(i);
+            Slots.getByte("Slot");
+            items.add(ItemStack.loadItemStackFromNBT(Slots).copy());
+        }
+        player.getEntityData().getCompoundTag("campInv").setCompoundTag(tagName, new NBTTagCompound());
+        return items;
+    }
+
     public static boolean isInstalled() {
         return isTheCampingModInstalled;
     }
