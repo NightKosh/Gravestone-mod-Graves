79a203928ecb3c08b688075a8f406612bd420a89
NightKosh <nightmare@railfox.ru>
2014-08-20 22:47:07 +0400

    fix mariculture compatibility

--- core/compatibility/GSCompatibilityMariculture.java
+++ core/compatibility/GSCompatibilityMariculture.java
@@ -1,11 +1,9 @@
 package gravestone.core.compatibility;
 
-import gravestone.GraveStoneLogger;
 import mariculture.api.core.MaricultureHandlers;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -23,20 +21,15 @@ public class GSCompatibilityMariculture {
 
     public static void addItems(List<ItemStack> items, EntityPlayer player) {
         if (isLoaded()) {
-            ItemStack[] mirrorItems = MaricultureHandlers.mirror.getMirrorContents(player);
-            if (mirrorItems != null) {
-                for (ItemStack item : mirrorItems) {
-                    if (item != null) {
-                        items.add(item.copy());
+            if (MaricultureHandlers.mirror != null) {
+                ItemStack[] mirrorItems = MaricultureHandlers.mirror.getMirrorContents(player);
+                if (mirrorItems != null) {
+                    for (ItemStack item : mirrorItems) {
+                        if (item != null) {
+                            items.add(item.copy());
+                        }
                     }
-                }
-
-                try {
-                    Class<?> mirrorHelperClass = Class.forName("mariculture.magic.MirrorHelper");
-                    Method saveMethod = mirrorHelperClass.getDeclaredMethod("save", EntityPlayer.class, ItemStack[].class);
-                    saveMethod.invoke(null, player, new ItemStack[4]);
-                } catch (Exception e) {
-                    GraveStoneLogger.logError("Can't clear mariculture mirror items!!!");
+                    MaricultureHandlers.mirror.setMirrorContents(player, new ItemStack[4]);
                 }
             }
         }
