f07b5a2ef5503b300347863337b58e61425ad129
Reshetnikov Dmitriy <nightkosh@gmail.com>
2015-08-17 15:58:22 +0300

    Some changes in graves crafting GUI, #190

--- src/main/java/gravestone/gui/GSChiselCraftingGui.java
+++ src/main/java/gravestone/gui/GSChiselCraftingGui.java
@@ -1,9 +1,16 @@
 package gravestone.gui;
 
+import gravestone.ModGraveStone;
+import gravestone.block.enums.EnumGraves;
+import gravestone.core.GSMessageHandler;
+import gravestone.core.Resources;
 import gravestone.gui.container.ChiselContainer;
+import gravestone.packets.ChiselMessageToServer;
+import gravestone.renderer.tileentity.TileEntityGSGraveStoneRenderer;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 /**
@@ -14,19 +21,54 @@ import org.lwjgl.opengl.GL11;
  */
 public class GSChiselCraftingGui extends GuiContainer {
 
-    private static final ResourceLocation craftingTableGuiTextures = new ResourceLocation("textures/gui/container/crafting_table.png");
+    private EntityPlayer player;
+
+    private final String graveButtonStr = "Gravestone";//ModGraveStone.proxy.getLocalizedString("gui.edit_grave.title");
+    private final String memorialButtonStr = "Memorial";//ModGraveStone.proxy.getLocalizedString("gui.edit_grave.close");
+    private final String typeStr = "Object to craft";//ModGraveStone.proxy.getLocalizedString("gui.edit_grave.close");
+
+    private GuiButton graveButton;
+    private GuiButton memorialButton;
 
     public GSChiselCraftingGui(InventoryPlayer inventoryPlayer) {
         super(new ChiselContainer(inventoryPlayer));
-        this.ySize = 114 + ChiselContainer.SLOT_WIDTH;
+        this.player = inventoryPlayer.player;
+    }
+
+    public void initGui() {
+//        this.buttonList.add(graveButton = new GuiButton(0, (width - xSize) / 2 + 100, (height - ySize) / 2 + 25, this.graveButtonStr));
+//        this.buttonList.add(memorialButton = new GuiButton(1, (width - xSize) / 2 + 100, (height - ySize) / 2 + 25, this.memorialButtonStr));
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
         GL11.glColor4f(1, 1, 1, 1);
-        this.mc.getTextureManager().bindTexture(craftingTableGuiTextures);
+        this.mc.renderEngine.bindTexture(Resources.INVENTORY);
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
         this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+
+//        this.drawString(this.fontRendererObj, this.typeStr, this.width / 2 - 40, (height - ySize) / 2 + 55, 16777215);
+
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+        TileEntityGSGraveStoneRenderer.instance.renderGrave((this.width / 2) - 150, (this.height / 2) - height, 40, player.worldObj, EnumGraves.ANDESITE_CROSS, false, false, false, null);
+    }
+
+    @Override
+    public void actionPerformed(GuiButton button) {
+        switch (button.id) {
+            case 0:
+                GSMessageHandler.networkWrapper.sendToServer(new ChiselMessageToServer(player, true));
+                break;
+            case 1:
+                GSMessageHandler.networkWrapper.sendToServer(new ChiselMessageToServer(player, false));
+                break;
+        }
+
     }
 }
--- src/main/java/gravestone/gui/container/ChiselContainer.java
+++ src/main/java/gravestone/gui/container/ChiselContainer.java
@@ -22,8 +22,8 @@ public class ChiselContainer extends Container {
     public static final int SLOT_WIDTH = 18;
     public static final int CRAFTING_SLOTS_COUNT = 4;
 
-    public InventoryCrafting craftMatrix = new InventoryCrafting(this, CRAFTING_SLOTS_COUNT, 1);
-    public IInventory craftResult = new InventoryCraftResult();
+//    public InventoryCrafting craftMatrix = new InventoryCrafting(this, CRAFTING_SLOTS_COUNT, 1);
+//    public IInventory craftResult = new InventoryCraftResult();
     private World world;
 
     private boolean isGravestone;
@@ -31,19 +31,19 @@ public class ChiselContainer extends Container {
     private EnumGraveMaterial material;
 
     public ChiselContainer(InventoryPlayer inventoryPlayer) {
-        int i = 2 * SLOT_WIDTH;
+//        int i = 2 * SLOT_WIDTH;
 
         world = inventoryPlayer.player.worldObj;
-        this.addSlotToContainer(new SlotCrafting(inventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 124, 35));
+//        this.addSlotToContainer(new SlotCrafting(inventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 35, 35));
 
         for (int row = 0; row < PLAYER_INVENTORY_ROWS_COUNT; row++) {
-            for (int column = 0; column < COLUMNS_COUNT; ++column) {
-                this.addSlotToContainer(new Slot(inventoryPlayer, column + row * COLUMNS_COUNT + COLUMNS_COUNT, 8 + column * SLOT_WIDTH, 103 + row * SLOT_WIDTH + i));
+            for (int column = 0; column < COLUMNS_COUNT; column++) {
+                this.addSlotToContainer(new Slot(inventoryPlayer, column + row * COLUMNS_COUNT + COLUMNS_COUNT, 8 + column * SLOT_WIDTH, 84 + row * SLOT_WIDTH));
             }
         }
 
         for (int column = 0; column < COLUMNS_COUNT; column++) {
-            this.addSlotToContainer(new Slot(inventoryPlayer, column, 8 + column * SLOT_WIDTH, 161 + i));
+            this.addSlotToContainer(new Slot(inventoryPlayer, column, 8 + column * SLOT_WIDTH, 142));
         }
     }
 
@@ -52,71 +52,71 @@ public class ChiselContainer extends Container {
         return true;
     }
 
-    @Override
-    public void onCraftMatrixChanged(IInventory inventoryIn) {
-        this.craftResult.setInventorySlotContents(0, GravesCraftingManager.getInstance().findMatchingRecipe(isGravestone, graveType, material, null));//TODO
-    }
-
-    @Override
-    public void onContainerClosed(EntityPlayer playerIn) {
-        super.onContainerClosed(playerIn);
-
-        if (!this.world.isRemote) {
-            for (int i = 0; i < CRAFTING_SLOTS_COUNT; i++) {
-                ItemStack itemstack = this.craftMatrix.getStackInSlotOnClosing(i);
-
-                if (itemstack != null) {
-                    playerIn.dropPlayerItemWithRandomChoice(itemstack, false);
-                }
-            }
-        }
-    }
-
-    @Override
-    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
-        ItemStack itemstack = null;
-        Slot slot = (Slot) this.inventorySlots.get(index);
-
-        if (slot != null && slot.getHasStack()) {
-            ItemStack itemstack1 = slot.getStack();
-            itemstack = itemstack1.copy();
-
-            if (index == 0) {
-                if (!this.mergeItemStack(itemstack1, 10, 46, true)) {
-                    return null;
-                }
-
-                slot.onSlotChange(itemstack1, itemstack);
-            } else if (index >= 10 && index < 37) {
-                if (!this.mergeItemStack(itemstack1, 37, 46, false)) {
-                    return null;
-                }
-            } else if (index >= 37 && index < 46) {
-                if (!this.mergeItemStack(itemstack1, 10, 37, false)) {
-                    return null;
-                }
-            } else if (!this.mergeItemStack(itemstack1, 10, 46, false)) {
-                return null;
-            }
-
-            if (itemstack1.stackSize == 0) {
-                slot.putStack((ItemStack) null);
-            } else {
-                slot.onSlotChanged();
-            }
-
-            if (itemstack1.stackSize == itemstack.stackSize) {
-                return null;
-            }
-
-            slot.onPickupFromSlot(playerIn, itemstack1);
-        }
-
-        return itemstack;
-    }
-
-    @Override
-    public boolean canMergeSlot(ItemStack p_94530_1_, Slot p_94530_2_) {
-        return p_94530_2_.inventory != this.craftResult && super.canMergeSlot(p_94530_1_, p_94530_2_);
-    }
+//    @Override
+//    public void onCraftMatrixChanged(IInventory inventoryIn) {
+//        this.craftResult.setInventorySlotContents(0, GravesCraftingManager.getInstance().findMatchingRecipe(isGravestone, graveType, material, null));//TODO
+//    }
+//
+//    @Override
+//    public void onContainerClosed(EntityPlayer playerIn) {
+//        super.onContainerClosed(playerIn);
+//
+//        if (!this.world.isRemote) {
+//            for (int i = 0; i < CRAFTING_SLOTS_COUNT; i++) {
+//                ItemStack itemstack = this.craftMatrix.getStackInSlotOnClosing(i);
+//
+//                if (itemstack != null) {
+//                    playerIn.dropPlayerItemWithRandomChoice(itemstack, false);
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+//        ItemStack itemstack = null;
+//        Slot slot = (Slot) this.inventorySlots.get(index);
+//
+//        if (slot != null && slot.getHasStack()) {
+//            ItemStack itemstack1 = slot.getStack();
+//            itemstack = itemstack1.copy();
+//
+//            if (index == 0) {
+//                if (!this.mergeItemStack(itemstack1, 10, 46, true)) {
+//                    return null;
+//                }
+//
+//                slot.onSlotChange(itemstack1, itemstack);
+//            } else if (index >= 10 && index < 37) {
+//                if (!this.mergeItemStack(itemstack1, 37, 46, false)) {
+//                    return null;
+//                }
+//            } else if (index >= 37 && index < 46) {
+//                if (!this.mergeItemStack(itemstack1, 10, 37, false)) {
+//                    return null;
+//                }
+//            } else if (!this.mergeItemStack(itemstack1, 10, 46, false)) {
+//                return null;
+//            }
+//
+//            if (itemstack1.stackSize == 0) {
+//                slot.putStack((ItemStack) null);
+//            } else {
+//                slot.onSlotChanged();
+//            }
+//
+//            if (itemstack1.stackSize == itemstack.stackSize) {
+//                return null;
+//            }
+//
+//            slot.onPickupFromSlot(playerIn, itemstack1);
+//        }
+//
+//        return itemstack;
+//    }
+//
+//    @Override
+//    public boolean canMergeSlot(ItemStack p_94530_1_, Slot p_94530_2_) {
+//        return p_94530_2_.inventory != this.craftResult && super.canMergeSlot(p_94530_1_, p_94530_2_);
+//    }
 }
