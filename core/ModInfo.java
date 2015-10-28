3276a0bf9491a664147982e4496a5caecba44371
NightKosh <nightkosh@gmail.com>
2015-10-28 14:27:24 +0300

    update version

--- core/ModInfo.java
+++ core/ModInfo.java
@@ -12,5 +12,5 @@ public class ModInfo {
     }
     public static final String ID = "GraveStone";
     public static final String NAME = "GraveStone";
-    public static final String VERSION = "2.12.3";
+    public static final String VERSION = "2.13.0";
 }
