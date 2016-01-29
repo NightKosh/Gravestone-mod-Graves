fd9b8868899d6c1882beda84d0e17a240b18c64e
NightKosh <nightkosh@gmail.com>
2016-01-29 15:39:43 +0300

    fix

--- src/main/java/nightkosh/gravestone/helper/IFog.java
+++ src/main/java/nightkosh/gravestone/helper/IFog.java
@@ -11,6 +11,15 @@ import net.minecraft.world.World;
  */
 public interface IFog {
 
+    //    public static final int FOG_RANGE = 30; TODO
+
     public default void addFog(World world, BlockPos pos) {
+        //TODO
+//        if (this.worldObj.isRemote && Config.isFogEnabled && TickEventHandler.getFogTicCount() == 0) {
+//                EntityPlayer player = this.worldObj.getClosestPlayer(this.pos.getX() + 0.5D, this.pos.getY() + 0.5D, this.pos.getZ() + 0.5D, FOG_RANGE);
+//                if (player != null && player.getCommandSenderEntity().equals(Minecraft.getMinecraft().thePlayer) && TimeHelper.isFogTime(this.worldObj)) {
+//                    RenderEventHandler.addFog();
+//                }
+//            }
     }
 }
