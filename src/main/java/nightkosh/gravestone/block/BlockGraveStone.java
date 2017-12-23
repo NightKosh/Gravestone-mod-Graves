cec1c2866129529576a559998e9dfd1c671abec3
NightKosh <nightkosh@gmail.com>
2017-12-23 16:39:38 +0300

    Fence connection, close #249

--- src/main/java/nightkosh/gravestone/block/BlockGraveStone.java
+++ src/main/java/nightkosh/gravestone/block/BlockGraveStone.java
@@ -7,6 +7,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -322,6 +323,11 @@ public class BlockGraveStone extends BlockContainer {
     @Override
     public boolean isFullCube(IBlockState state) {
         return false;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess access, IBlockState state, BlockPos pos, EnumFacing facing) {
+        return BlockFaceShape.UNDEFINED;
     }
 
     @Override
