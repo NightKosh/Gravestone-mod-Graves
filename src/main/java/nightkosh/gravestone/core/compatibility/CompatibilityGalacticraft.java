873633e934fbaba72e211b36ca9c6b9b6472e5c9
NightKosh <nightkosh@gmail.com>
2018-10-19 18:36:45 +0300

    Fix for #289

--- src/main/java/nightkosh/gravestone/core/compatibility/CompatibilityGalacticraft.java
+++ src/main/java/nightkosh/gravestone/core/compatibility/CompatibilityGalacticraft.java
@@ -1,12 +1,8 @@
 package nightkosh.gravestone.core.compatibility;
 
-import micdoodle8.mods.galacticraft.api.inventory.AccessInventoryGC;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import nightkosh.gravestone.api.GraveStoneAPI;
 import nightkosh.gravestone.config.Config;
-import nightkosh.gravestone.core.logger.GSLogger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,18 +21,18 @@ public class CompatibilityGalacticraft implements ICompatibility {
         if (isModLoaded(MOD_ID) && Config.storeGalacticraftItems) {
             GraveStoneAPI.graveGenerationAtDeath.addPlayerItemsHandler((player, source) -> {
                 List<ItemStack> items = new ArrayList<>();
-                IInventory inventory = AccessInventoryGC.getGCInventoryForPlayer((EntityPlayerMP) player);
-                if (inventory != null) {
-                    for (int slot = 0; slot < inventory.getSizeInventory(); slot++) {
-                        ItemStack stack = inventory.getStackInSlot(slot);
-                        if (stack != null && !stack.isEmpty()) {
-                            items.add(stack.copy());
-                            inventory.setInventorySlotContents(slot, ItemStack.EMPTY);
-                        }
-                    }
-                } else {
-                    GSLogger.logError("Can't save Galacticraft items!!!");
-                }
+//                IInventory inventory = AccessInventoryGC.getGCInventoryForPlayer((EntityPlayerMP) player);
+//                if (inventory != null) {
+//                    for (int slot = 0; slot < inventory.getSizeInventory(); slot++) {
+//                        ItemStack stack = inventory.getStackInSlot(slot);
+//                        if (stack != null && !stack.isEmpty()) {
+//                            items.add(stack.copy());
+//                            inventory.setInventorySlotContents(slot, ItemStack.EMPTY);
+//                        }
+//                    }
+//                } else {
+//                    GSLogger.logError("Can't save Galacticraft items!!!");
+//                }
                 return items;
             });
         }
