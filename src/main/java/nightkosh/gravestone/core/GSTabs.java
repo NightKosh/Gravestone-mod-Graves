b1f3d51979d62864fd9c64015e81a3acce895723
NightKosh <nightkosh@gmail.com>
2025-12-10 17:14:35 +0100

    Fix tabs

--- src/main/java/nightkosh/gravestone/core/GSTabs.java
+++ src/main/java/nightkosh/gravestone/core/GSTabs.java
@@ -3,8 +3,10 @@ package nightkosh.gravestone.core;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import nightkosh.gravestone.api.GraveStoneAPI;
 import nightkosh.gravestone.api.ModInfo;
 
@@ -14,6 +16,7 @@ import nightkosh.gravestone.api.ModInfo;
  * @author NightKosh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
+@Mod.EventBusSubscriber(modid = ModInfo.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GSTabs {
 
     public static CreativeModeTab GS_TAB;
@@ -23,8 +26,7 @@ public class GSTabs {
         GS_TAB = event.registerCreativeModeTab(
                 ResourceLocation.fromNamespaceAndPath(ModInfo.ID, "sophisticated_wolves"),
                 builder -> builder
-//                        .icon(() -> new net.minecraft.world.item.ItemStack(GSBlock. ()))
-//        return new ItemStack(GSBlock.GRAVE_STONE, 1, EnumGraves.STONE_VERTICAL_PLATE.ordinal());
+                        .icon(() -> new ItemStack(GSBlocks.getGraveStone()))
                         .title(Component.translatable("itemGroup." + ModInfo.ID))
                 .build()
         );
@@ -35,7 +37,7 @@ public class GSTabs {
     @SubscribeEvent
     public static void buildContents(CreativeModeTabEvent.BuildContents event) {
         if (event.getTab() == GS_TAB) {
-//            event.accept();
+            event.accept(new ItemStack(GSBlocks.getGraveStone()));
         }
     }
 
